7d63a68602bad1296191c7e24002f90e9595e8bb
Balaji Segu Krishnaiah <60575418+BALAJI-SK@users.noreply.github.com>
2026-06-20 12:06:49 +0100

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l=0,h=s.size()-1;int n =h+1;
+
+        while(l<h){
+            while(l<h && !isalnum(s[l]))l++;
+    
+            while(l<h && !isalnum(s[h]))h--;
+         if (tolower(s[l]) != tolower(s[h])) return false;
+            l++;h--;
+
+        }
+        return true;
+    }
+};
