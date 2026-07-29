0a62691e3f0e0332faab9e2d57c7fe46d0c85350
Balaji Segu Krishnaiah <60575418+BALAJI-SK@users.noreply.github.com>
2026-07-29 01:39:24 +0100

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,39 @@
+class TimeMap {
+    private:
+    unordered_map<string, vector<pair<int,string>>>umap;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        if(umap.find(key)!= umap.end()){
+            umap[key].push_back({timestamp,value});
+        }else{
+            umap[key]={};
+                 umap[key].push_back({timestamp,value});
+        }
+    }
+ 
+    string get(string key, int timestamp) {
+        auto & arr = umap[key];
+
+        int l = 0,h=arr.size()-1,ans=-1;
+        while(l<=h){
+            // cout << l << " "<< h << " "<< ans <<endl;
+            int mid = (h+l)/2;
+            if(arr[mid].first==timestamp)return arr[mid].second;
+            if(arr[mid].first<timestamp){
+                l=mid+1;
+                ans=mid;
+                
+            }else{
+                h=mid-1;
+            }
+          // cout << l << " "<< h << " "<< ans <<endl;
+        }
+        return ans ==-1 || ans==arr.size() ?"": arr[ans].second;
+        // return "bh";
+        // return lb==arr.end()?"":lb->second;
+    }
+};
